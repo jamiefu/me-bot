9756d54b181fc49335ab5adbc6a903eaccd53c00
Jamie Fu <jamie.fu@gmail.com>
2020-06-22 19:49:54 -0400

    all the things

---
+++ app.py
@@ -0,0 +1,43 @@
+from models.encoder_decoder import MessageDataset, EncoderRNN, AttnDecoderRNN, evaluate, play
+
+import torch
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+DATASET_PATH = "./message_dataset.pth"
+DATALOADER_PATH = "./message_dataloader.pth"
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Device is {}".format(device))
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/text', methods=["GET"])
+def respond():
+    text = request.json["text"]
+    response = play(text, encoder, decoder, dataset)
+    return jsonify(response)
+
+dataset = torch.load(DATASET_PATH)
+vocab_words = dataset.word_num
+max_length = dataset.max_message
+dataloader = torch.load(DATALOADER_PATH)
+print("Loaded dataset and dataloader from paths")
+
+hidden_size = 256
+encoder = EncoderRNN(vocab_words, hidden_size).to(device)
+decoder = AttnDecoderRNN(hidden_size, vocab_words, max_length, dropout_p=0.1).to(device)
+
+temp_path = "./bot_temp_v2.pth"
+final_path = "./bot_v2.pth"
+
+checkpoint = torch.load(temp_path, map_location=torch.device('cpu'))
+encoder.load_state_dict(checkpoint["encoder"])
+decoder.load_state_dict(checkpoint["decoder"])
+
+if __name__ == "__main__":
+    app.run()
+
