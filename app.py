8f495e68d289db68b32d6d4ce0af8486e376f4c2
Jamie Fu <jamie.fu@gmail.com>
2020-06-22 22:45:00 -0400

    now i'm just trolling f

--- app.py
+++ app.py
@@ -25,27 +25,28 @@ def respond():
     answer["response"] = response
     return jsonify(answer)
 
-dataset = torch.load(DATASET_PATH)
-vocab_words = dataset.word_num
-max_length = dataset.max_message
-
-# vocab_words = 20202
-# max_length = 801
-# dataloader = torch.load(DATALOADER_PATH)
-# print("Loaded dataset and dataloader from paths")
-
-hidden_size = 256
-encoder = EncoderRNN(vocab_words, hidden_size).to(device)
-decoder = AttnDecoderRNN(hidden_size, vocab_words, max_length, dropout_p=0.1).to(device)
-
-temp_path = "./bot_temp_v2.pth"
-final_path = "./bot_v2.pth"
-
-checkpoint = torch.load(temp_path, map_location=torch.device('cpu'))
-encoder.load_state_dict(checkpoint["encoder"])
-decoder.load_state_dict(checkpoint["decoder"])
-
 if __name__ == "__main__":
     from encoder_decoder import *
+
+    dataset = torch.load(DATASET_PATH)
+    vocab_words = dataset.word_num
+    max_length = dataset.max_message
+
+    # vocab_words = 20202
+    # max_length = 801
+    # dataloader = torch.load(DATALOADER_PATH)
+    # print("Loaded dataset and dataloader from paths")
+
+    hidden_size = 256
+    encoder = EncoderRNN(vocab_words, hidden_size).to(device)
+    decoder = AttnDecoderRNN(hidden_size, vocab_words, max_length, dropout_p=0.1).to(device)
+
+    temp_path = "./bot_temp_v2.pth"
+    final_path = "./bot_v2.pth"
+
+    checkpoint = torch.load(temp_path, map_location=torch.device('cpu'))
+    encoder.load_state_dict(checkpoint["encoder"])
+    decoder.load_state_dict(checkpoint["decoder"])
+    
     app.run()
 
